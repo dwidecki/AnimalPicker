aa02b9b405dccaef15084118e92f8d80c0fbeb3c
dwidecki <dylan.widecki@quinnipiac.edu>
2022-02-26 15:35:27 -0500

    asks user for input until valid

--- src/animalPicker.java
+++ src/animalPicker.java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 public class animalPicker {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		boolean valid = false;
+		while (!valid) {
 		System.out.println("Would you like to see: a dog or a cat?");
 		String userInput = scanner.next();
 
 		if (userInput.equalsIgnoreCase("dog")) {
+			valid = true;
 			System.out.println("DOG ASCII ART HERE");
 		}
 
 		else if (userInput.equalsIgnoreCase("cat")) {
+			valid = true;
 			System.out.println("CAT ASCII ART HERE");
 		}
 		else {
 			System.out.println("ERROR: Invalid Input!");
+		}
 		}
 	}
 
