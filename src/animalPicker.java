4759ec03d6fa57d78e5aacf7e9d4e3c3c404551c
dwidecki <dylan.widecki@quinnipiac.edu>
2022-02-26 15:27:33 -0500

    asks user to choose cat or dog

--- src/animalPicker.java
+++ src/animalPicker.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 
 public class animalPicker {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Would you like to see: a dog or a cat?");
+		String userInput = scanner.next();
+
+		if (userInput.equalsIgnoreCase("dog")) {
+			System.out.println("DOG ASCII ART HERE");
+		}
+
+		else if (userInput.equalsIgnoreCase("cat")) {
+			System.out.println("CAT ASCII ART HERE");
+		}
 		
 	}
 
